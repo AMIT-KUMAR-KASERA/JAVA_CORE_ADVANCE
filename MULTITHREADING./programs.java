8520cffb70a0c813e11f8e64d376d497c9b90221
AMIT KUMAR KASERA <106943697+AMIT-KUMAR-KASERA@users.noreply.github.com>
2025-08-01 17:50:56 +0530

    Update programs.java

--- MULTITHREADING./programs.java
+++ MULTITHREADING./programs.java
@@ -2199,8 +2199,270 @@ public class RestaurantSystem {
 }
 
 
+======================================================================================================================================================================================
+																				01-08-2025
+
+	
 
 
+Develop a program by using ITC, where two customers called Depositor and Drawer
+will deposit and withdraw the money respectively from the ATM machine.
+
+Create a BLC class called Account
+    Instance Variable :
+    private int balance = 0; (Initial Balance is 0)
+    
+    Methods :
+    ---------
+    1)
+    Create a synchronized method withdraw :
+    Name of the method : withdraw
+    modifier : public 
+    Parameter : int amount
+    
+    Inside this method write the logic to verify that current balance should not
+    be less than withdraw amount by using while loop, if the current balance is 
+    less than withdraw amount then put the current thread into wait method.
+    
+    Inside the method write the logic to withdraw the amount if balance is 
+    not less than withdraw amount and print the updated balance.
+    
+    notify any other thread which are waiting on the same object for the lock.
+    
+    2) Create a synchronized method deposit
+    Name of the method : deposit
+    modifier : public 
+    Parameter : int amount
+    
+    In this method write the logic to deposit the amount, print the updated
+    balance, notify the waiting thread which is waiting for withdraw the 
+    amount.
+    
+ Create another BLC class Drawer which extends from Thread class
+    Instance Variable :
+    private Account account;  //HAS-A Relation
+    
+    Initialize the Object properties by using parameterized constructor.
+    
+    Override run() method :
+    inside run method create an array variable of type int withdrawals which 
+    will accept multiple amount in int value.
+    
+    create a for each loop to accept these integer value one by one and pass these 
+    value to withdraw the amount. In each withdraw process you should have 1 sec
+    waiting period.
+    
+ Create another BLC class Depositor which extends from Thread class
+    Instance Variable :
+    private Account account;  //HAS-A Relation
+    
+    Initialize the Object properties by using parameterized constructor.
+    
+    Override run() method :
+    inside run method create an array variable of type int deposits which 
+    will accept multiple amount in int value.
+    
+    create a for each loop to accept these integer value one by one and pass these 
+    value to deposit the amount. In each deposit process you should have 1 sec
+    waiting period.
+    
+ Create an ELC class ATMMachine, inside main method create object as per 
+ requirement, start both the threads to see inter thread communication result
+ --------------------------------------------------------------------------------------------
+ 1.	What is the output of the following code
+public class MultiThread implements Runnable
+{
+	public static void main(String[] args) throws InterruptedException {
+		Thread thread = new Thread(new MultiThread());
+		thread.start();
+		System.out.print("nit ");
+		thread.join();
+		System.out.println("google ");
+	}
+	@Override
+	public void run() {
+		System.out.println("NareshIT");
+	}
+}
+
+
+2.	What is the output of the following code
+class Test extends Thread
+{
+	@Override
+	public void run() {
+		System.out.println("Run");
+		super.run();
+	}
+	
+	@Override
+	public synchronized void start() {
+		System.out.println("Start");
+		super.start();
+	}	
+}
+public class MultiThread
+{
+	public static void main(String[] args) {
+		nit();
+	}
+	
+	static public void nit() {
+		Test test = new Test();
+		test.start();
+	}
+}
+
+
+3.	What is the output of the following code
+class Test extends Thread
+{
+	int a;
+	public void run() {
+		a++;
+	}
+}
+public class MultiThread
+{
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.run();
+		System.out.println(test.a);
+		test.start();
+		System.out.println(test.a);
+		System.out.println(test.isAlive());
+		System.out.println(test.getName());		
+	}
+}
+
+
+4.	What is the output of the following code
+public class MultiThread extends Thread
+{
+	public void run() {
+		System.out.println(Thread.currentThread().getId());
+	}
+	public static void main(String[] args) {
+		MultiThread nit = new MultiThread();
+		for(int i=0;i<10;i++)
+		{
+			nit.start();
+		}
+	}
+}
+
+
+5.	What is the output of the following code
+public class MultiThread extends Thread
+{
+	public void run() {
+		System.out.print(Thread.currentThread().getId()+" ");
+	}
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++)
+		{
+			MultiThread nit = new MultiThread();
+			nit.start();
+		}
+	}
+}
+
+
+6.	What is the output of the following code
+public class MultiThread extends Thread
+{
+	public void run() {
+		System.out.print(Thread.currentThread().getId()+" ");
+	}
+	
+	@Override
+	public void start() {
+		System.out.print(Thread.currentThread().getName()+" ");
+	}
+	
+	public static void main(String[] args) {
+		MultiThread nit = new MultiThread();
+		for(int i=0;i<10;i++)
+		{
+			nit.start();
+		}
+	}
+}
+
+7.	What is the output of the following code
+public class MultiThread extends Thread
+{
+	public void run() {
+		System.out.print(Thread.currentThread().getId()+" ");
+	}
+	
+	@Override
+	public void start() {
+		System.out.print(Thread.currentThread().getName()+" ");
+	}
+	
+	public static void main(String[] args) {
+		MultiThread nit = new MultiThread();
+		for(int i=0;i<10;i++)
+		{
+			nit.run();
+		}
+	}
+}
+
+8.	What is the output of the following program
+public class MultiThread implements Runnable
+{
+	Thread thread;
+	public MultiThread() {
+		thread = new Thread(this);
+		thread.start();
+	}
+	public void run() {
+		System.out.println(thread);
+		
+	}	
+	public static void main(String[] args) {
+		new MultiThread();
+	}
+}
+
+
+9.	What is the output of the following program
+public class MultiThread implements Runnable
+{
+	Thread thread;
+	public MultiThread() {
+		thread = new Thread(this,"NareshIT");
+		thread.setPriority(6);
+		thread.start();
+	}
+	public void run() {
+		System.out.println(thread);
+		
+	}
+	public static void main(String[] args) {
+		new MultiThread();
+	}
+}
+
+
+10.	What is the output of the following program
+public class MultiThread extends Thread
+{	
+	public static void main(String[] args) {
+		MultiThread m1 = new MultiThread();
+		MultiThread m2 = new MultiThread();
+		MultiThread m3 = new MultiThread();
+		m1.start();
+		m2.start();
+		m3.start();
+	}
+	
+public void run() {
+		System.out.println(Thread.currentThread().getName());
+	}
+}
 
 
 
